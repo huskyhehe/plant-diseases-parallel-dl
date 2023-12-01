d3408c8bc1811df6f20b8bcaf61bdcf85df8eaab
huskyhehe <97263406+huskyhehe@users.noreply.github.com>
2023-12-01 15:38:52 -0500

    Create torchsum.py

---
+++ torchsum.py
@@ -0,0 +1,9 @@
+        b, c, h, w = images.shape
+        nb_pixels = b * h * w
+        sum_ = torch.sum(images, dim=[0, 2, 3])
+        sum_of_square = torch.sum(images ** 2, dim=[0, 2, 3])
+        fst_moment = (cnt * fst_moment + sum_) / (cnt + nb_pixels)
+        snd_moment = (cnt * snd_moment + sum_of_square) / (cnt + nb_pixels)
+        cnt += nb_pixels
+        
+    mean, std = fst_moment, torch.sqrt(snd_moment - fst_moment ** 2)
